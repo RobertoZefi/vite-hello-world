94abb772ab1eabee3b74f02c75deb62d5acbbb08
RobertoZefi <zefi.roberto2001@gmail.com>
2023-02-28 17:04:00 +0100

    Done + bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,41 @@
+<script>
+export default{
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="jumbotron">
+        <img src="../assets/itachi-wallpaper-fldq4uzhzz2eubvj-fldq4uzhzz2eubvj.jpg" alt="">
+        <p class="description">jumbotron</p>
+    </div>
+</template>
+
+<style>
+.jumbotron img{
+    max-width: 100%;
+    padding-top: 30px;
+}
+
+.jumbotron{
+    position: relative;
+}
+
+.description{
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    border: 2px solid white;
+    border-radius: 20px;
+    padding: 20px;
+    font-size: 40px;
+    text-transform: uppercase;
+    background-color: white;
+    font-weight: bold;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+export default{
+    data(){
+        return{
+            message: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="title">
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<style>
+    .title{
+    border: 2px solid black;
+    padding: 30px;
+    border-radius: 20px;
+    }
+</style>
